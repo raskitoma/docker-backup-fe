31a292ce909dc72963aab48da0c15dcc4a275b5d
Elias Achi <43652862+raskitoma@users.noreply.github.com>
2026-01-21 11:37:48 -0500

    Fix docker read

--- backend/app/docker_reader.py
+++ backend/app/docker_reader.py
@@ -1,13 +1,29 @@
 import docker
 
-client = docker.from_env()
+def get_docker_client():
+    try:
+        return docker.DockerClient(base_url="unix:///var/run/docker.sock")
+    except Exception as e:
+        print("Docker connection failed:", e)
+        return None
+
 
 def get_running_containers():
+    client = get_docker_client()
+
+    if not client:
+        return []
+
+    containers = client.containers.list()
+
     result = []
-    for c in client.containers.list(all=True):
+
+    for c in containers:
         result.append({
+            "id": c.short_id,
             "name": c.name,
-            "status": c.status,
-            "image": c.image.tags
+            "image": c.image.tags[0] if c.image.tags else "unknown",
+            "status": c.status
         })
+
     return result
